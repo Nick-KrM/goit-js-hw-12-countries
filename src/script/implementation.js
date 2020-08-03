a5c5bfd84558600275e944af4cce62d511b5f2c1
Nick <nikitakrm87@gmail.com>
2020-08-03 21:17:38 +0300

    added structure & import

---
+++ src/script/implementation.js
@@ -0,0 +1,6 @@
+import refs from './refs';
+import './fetchCountry';
+import countriesList from '../templates/countriesList.hbs';
+import resultCountry from '../templates/resultCountry.hbs';
+
+// console.dir(resultCountry);
