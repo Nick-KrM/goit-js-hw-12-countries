3d0d50383d63e11d6833d686cdedf3bc68c252a6
Nick <nikitakrm87@gmail.com>
2020-08-06 21:39:45 +0300

    first submit, without styles

--- src/script/implementation.js
+++ src/script/implementation.js
@@ -1,47 +1,7 @@
-// import refs from './refs';
-import fetchCountries from './fetchCountry';
-import countriesList from '../templates/countriesList.hbs';
-import resultCountry from '../templates/resultCountry.hbs';
-import errorTpl from '../templates/errorWarning.hbs';
+import markup from './markup';
+import refs from './refs';
+
 
 const debounce = require('lodash.debounce');
-
-const refs = {
-    input: document.getElementById('inputlg'),
-    results: document.querySelector('.results'),
-};
-
-function clearReults() {
-    refs.results.innerHTML = '';
-};
-
-function markup(e) {
-    const textInInput = e.target.value;
-    const url = `https://restcountries.eu/rest/v2/name/${textInInput}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            // const markup = countriesList(data);
-            console.log(data)
-
-            if (data.length > 10) {
-                refs.results.innerHTML = errorTpl();
-                setTimeout(() => { clearReults() }, 2000);
-                return
-            };
-
-            if (data.length === 1) {
-                refs.results.innerHTML = resultCountry(data);
-                return
-            };
-
-            if (data.length > 1 && data.length <= 10) {
-                refs.results.innerHTML = countriesList(data);
-                return
-            };
-        })
-        .catch(error => console.log(error));
-
-};
 
 refs.input.addEventListener('input', debounce(markup, 500));
---
+++ src/script/markup.js
@@ -0,0 +1,56 @@
+import countriesList from '../templates/countriesList.hbs';
+import resultCountry from '../templates/resultCountry.hbs';
+import IncompleteRequest from '../templates/IncompleteRequest.hbs';
+import incorrectRequest from '../templates/incorrectRequest.hbs';
+import refs from './refs';
+
+
+// function fetchCountries(inputValue) {
+//     const url = `https://restcountries.eu/rest/v2/name/${inputValue}?fields=name;population;flag;languages;capital`;
+
+//     return fetch(url)
+//         .then(res => res.json())
+//         .catch(error => console.log(error));
+
+// }
+
+function markup(e) {
+    e.preventDefault();
+    const inputValue = e.target.value;
+    const url = `https://restcountries.eu/rest/v2/name/${inputValue}`;
+
+    return fetch(url)
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            } else {
+                refs.results.innerHTML = incorrectRequest();
+            }
+        })
+        .then(data => {
+            refs.results.innerHTML = '';
+
+            if (data.length > 10) {
+                refs.results.insertAdjacentHTML('beforeend', IncompleteRequest());
+                return
+            };
+
+            if (data.length === 1) {
+                refs.results.insertAdjacentHTML('beforeend', resultCountry(data));
+                return
+            };
+
+            if (data.length) {
+                refs.results.insertAdjacentHTML('beforeend', countriesList(data));
+                return
+            };
+        })
+        .catch(() => {
+            if (inputValue !== '')
+                refs.results.innerHTML = incorrectRequest();
+            console.log('some error')
+        });
+
+};
+
+export default markup;
